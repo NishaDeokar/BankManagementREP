9c6a90e48147fbf0f0a5a7e75537bbe55c183022
NishaDeokar <nishadeokar129@gmail.com>
2025-03-13 19:59:27 +0530

    Update Test.java
    pull

--- GitDemo/src/Task/Test.java
+++ GitDemo/src/Task/Test.java
@@ -4,17 +4,19 @@ public class Test
 {
 	public void m1()
 	{
-		
+		System.out.println("M1 Method");
 	}
 	
 	public void m2()
 	{
-		
+		System.out.println("M2 Method");
 	}
 	
 	public static void main(String[] args)
 	{
-		
+		Test t = new Test();
+		t.m1();
+		t.m2();
 	}
 	
 	
